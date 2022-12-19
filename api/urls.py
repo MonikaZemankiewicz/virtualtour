286b92069f1b41b71986b5079ef3f39a05dbc14d
MonikaZemankiewicz <30733275+MonikaZemankiewicz@users.noreply.github.com>
2022-12-19 03:30:48 +0000

    Class based API views

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from . views import video_list, video_details
+# from . views import video_list, video_details
+from .views import VideoList, VideoDetails
 
 
 urlpatterns = [
-    path('videos/', video_list),
-    path('videos/<int:pk>/', video_details),
+    path('videos/', VideoList.as_view()),
+    path('videos/<int:id>/', VideoDetails.as_view()),
+
+
+
+    # path('videos/', video_list),
+    # path('videos/<int:pk>/', video_details),
 ]
--- api/views.py
+++ api/views.py
@@ -6,7 +6,57 @@ from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import APIView
+from rest_framework import generics
+from rest_framework import mixins
 
+
+# class based API Views
+class VideoList(APIView):
+
+    def get(self, request):
+        videos = Video.objects.all()
+        serializer = VideoSerializer(videos, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = VideoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class VideoDetails(APIView):
+
+    def get_object(self, id):
+        try:
+            return Video.objects.get(id=id)
+
+        except Video.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        video = self.get_object(id)
+        serializer = VideoSerializer(video)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        video = self.get_object(id)
+        serializer = VideoSerializer(video, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        video = self.get_object(id)
+        video.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Function based API views
+'''
 
 @api_view(['GET', 'POST'])
 def video_list(request):
@@ -47,3 +97,4 @@ def video_details(request, pk):
     elif request.method == 'DELETE':
         video.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+'''
