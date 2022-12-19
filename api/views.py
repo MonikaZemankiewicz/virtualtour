24593f44c23e48ee15d9d8ad68f45b5b64e7c343
MonikaZemankiewicz <30733275+MonikaZemankiewicz@users.noreply.github.com>
2022-12-19 03:46:13 +0000

    Mixins

--- api/views.py
+++ api/views.py
@@ -11,90 +11,29 @@ from rest_framework import generics
 from rest_framework import mixins
 
 
-# class based API Views
-class VideoList(APIView):
+class VideoList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
 
     def get(self, request):
-        videos = Video.objects.all()
-        serializer = VideoSerializer(videos, many=True)
-        return Response(serializer.data)
+        return self.list(request)
 
     def post(self, request):
-        serializer = VideoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.create(request)
 
 
-class VideoDetails(APIView):
+class VideoDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
 
-    def get_object(self, id):
-        try:
-            return Video.objects.get(id=id)
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
 
-        except Video.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+    lookup_field = 'id'
 
     def get(self, request, id):
-        video = self.get_object(id)
-        serializer = VideoSerializer(video)
-        return Response(serializer.data)
+        return self.retrieve(request, id=id)
 
     def put(self, request, id):
-        video = self.get_object(id)
-        serializer = VideoSerializer(video, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.update(request, id=id)
 
     def delete(self, request, id):
-        video = self.get_object(id)
-        video.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-# Function based API views
-'''
-
-@api_view(['GET', 'POST'])
-def video_list(request):
-
-    # get all articles
-    if request.method == 'GET':
-        videos = Video.objects.all()
-        serializer = VideoSerializer(videos, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = VideoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def video_details(request, pk):
-    try:
-        video = Video.objects.get(pk=pk)
-
-    except Video.DoesNotExist:
-        return Response(statu=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = VideoSerializer(video)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = VideoSerializer(video, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        video.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-'''
+        return self.destroy(request, id=id)
