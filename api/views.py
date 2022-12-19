d167634f54c9a6060e3a1d0f7c78e92c5b2e8d7c
MonikaZemankiewicz <30733275+MonikaZemankiewicz@users.noreply.github.com>
2022-12-19 18:53:56 +0000

    Model View Set

--- api/views.py
+++ api/views.py
@@ -12,65 +12,6 @@ from rest_framework import mixins
 from rest_framework import viewsets
 
 
-class VideoViewSet(viewsets.ViewSet):
-
-    def list(self, request):
-        videos = Video.objects.all()
-        serializer = VideoSerializer(videos, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = VideoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        queryset = Video.objects.all()
-        video = get_object_or_404(queryset, pk=pk)
-        serializer = VideoSerializer(video)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        video = Video.objects.get(pk=pk)
-        serializer = VideoSerializer(video, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        video = Video.objects.get(pk=pk)
-        video.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-'''
-class VideoList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
-
-    def get(self, request):
-        return self.list(request)
-
-    def post(self, request):
-        return self.create(request)
-
-
-class VideoDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
-
-    queryset = Video.objects.all()
-    serializer_class = VideoSerializer
-
-    lookup_field = 'id'
-
-    def get(self, request, id):
-        return self.retrieve(request, id=id)
-
-    def put(self, request, id):
-        return self.update(request, id=id)
-
-    def delete(self, request, id):
-        return self.destroy(request, id=id)
-'''
