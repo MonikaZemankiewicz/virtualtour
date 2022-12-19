95724edcf2bfa6ea72307cead13d133957a57c98
MonikaZemankiewicz <30733275+MonikaZemankiewicz@users.noreply.github.com>
2022-12-19 02:47:05 +0000

    Function based API view

--- api/views.py
+++ api/views.py
@@ -3,44 +3,47 @@ from .models import Video
 from .serializers import VideoSerializer
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def video_list(request):
 
     # get all articles
     if request.method == 'GET':
         videos = Video.objects.all()
         serializer = VideoSerializer(videos, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
+
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = VideoSerializer(data=data)
+        serializer = VideoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def video_details(request, pk):
     try:
         video = Video.objects.get(pk=pk)
 
     except Video.DoesNotExist:
-        return HttpResponse(statu=404)
+        return Response(statu=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = VideoSerializer(video)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
+
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = VideoSerializer(video, data=data)
+        serializer = VideoSerializer(video, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     elif request.method == 'DELETE':
         video.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
