d34433fdc35785411e4a7b2a80985a95a9235b4f
MonikaZemankiewicz <30733275+MonikaZemankiewicz@users.noreply.github.com>
2022-12-19 04:10:59 +0000

    View set

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,19 @@
-from django.urls import path
-# from . views import video_list, video_details
-from .views import VideoList, VideoDetails
+from django.urls import path, include
+from .views import VideoViewSet
+from rest_framework.routers import DefaultRouter
 
+# from . views import video_list, video_details
+# from .views import VideoList, VideoDetails
+
+router = DefaultRouter()
+router.register('videos', VideoViewSet, basename='videos')
 
 urlpatterns = [
-    path('videos/', VideoList.as_view()),
-    path('videos/<int:id>/', VideoDetails.as_view()),
+    path('', include(router.urls)),
 
 
-
+    # path('videos/', VideoList.as_view()),
+    # path('videos/<int:id>/', VideoDetails.as_view()),
     # path('videos/', video_list),
     # path('videos/<int:pk>/', video_details),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import get_object_or_404, render, HttpResponse
 from .models import Video
 from .serializers import VideoSerializer
 from django.http import JsonResponse
@@ -9,8 +9,44 @@ from rest_framework import status
 from rest_framework.decorators import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import viewsets
 
 
+class VideoViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        videos = Video.objects.all()
+        serializer = VideoSerializer(videos, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = VideoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = Video.objects.all()
+        video = get_object_or_404(queryset, pk=pk)
+        serializer = VideoSerializer(video)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        video = Video.objects.get(pk=pk)
+        serializer = VideoSerializer(video, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        video = Video.objects.get(pk=pk)
+        video.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+'''
 class VideoList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
@@ -37,3 +73,4 @@ class VideoDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.Up
 
     def delete(self, request, id):
         return self.destroy(request, id=id)
+'''
