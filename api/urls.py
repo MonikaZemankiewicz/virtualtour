573679ff290d337c6fe9ffab7aeeef2d96dd5ea5
MonikaZemankiewicz <30733275+MonikaZemankiewicz@users.noreply.github.com>
2023-01-23 02:50:43 +0000

    virtual tour api route

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import VideoViewSet, UserViewSet, ImageViewSet
+from .views import VideoViewSet, UserViewSet, ImageViewSet, VirtualTourViewSet
 from rest_framework.routers import DefaultRouter
 from django.conf.urls.static import static
 from django.conf import settings
@@ -8,6 +8,7 @@ from django.conf import settings
 router = DefaultRouter()
 router.register('videos', VideoViewSet, basename='videos')
 router.register('images', ImageViewSet, basename='images')
+router.register('images', VirtualTourViewSet, basename='virtualtours')
 router.register('users', UserViewSet)
 
 urlpatterns = [
