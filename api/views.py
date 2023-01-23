9eb8e9a744c2be3b461ff4c781ac27c50e9cb5f5
MonikaZemankiewicz <30733275+MonikaZemankiewicz@users.noreply.github.com>
2023-01-23 02:49:15 +0000

    custom auth token - just the class, still using default one

--- api/views.py
+++ api/views.py
@@ -9,21 +9,21 @@ from django.contrib.auth.models import User
 class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     authentication_classes = (TokenAuthentication,)
 
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     authentication_classes = (TokenAuthentication,)
 
 
 class VirtualTourViewSet(viewsets.ModelViewSet):
     queryset = VirtualTour.objects.all()
     serializer_class = VirtualTourSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     authentication_classes = (TokenAuthentication,)
 
 
